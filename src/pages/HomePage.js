83b10bcb1bed154f345462aaa7c2d7b52f5e71aa
Pedro Matsubara <pedro.matsubara.pereira@gmail.com>
2024-11-16 18:19:23 -0300

    feat(Home): Add nav buttons on HomePage component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,43 @@
-import React from "react";
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <Container maxWidth="md" sx={{ paddingY: 4 }}>
       <Box mb={3}>
         <Typography variant="h4" textAlign="center">
-          Home Page
+          Welcome to Dunder Mifflin Management App
         </Typography>
+      </Box>
+      <Box display="flex" justifyContent="center" gap={2}>
+        <Button
+          color="inherit"
+          component={Link}
+          to="/employees"
+          size="large"
+          sx={styles.largeBtn}
+        >
+          List Employees
+        </Button>
+        <Button
+          color="inherit"
+          component={Link}
+          to="/create-employee"
+          size="large"
+          sx={styles.largeBtn}
+        >
+          Add Employee
+        </Button>
       </Box>
     </Container>
   );
+};
+
+const styles = {
+  largeBtn: {
+    fontSize: "1.5rem",
+    padding: "16px 32px",
+  },
 };
 
 export default HomePage;
